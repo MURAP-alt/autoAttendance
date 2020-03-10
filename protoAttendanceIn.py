e31863e7e743a18c9159a18a2f61123cadfc3881
村岡 太一 <taichi.muraoka@coworks.local>
2020-03-10 11:49:35 +0900

    出勤用プロトタイプ

---
+++ protoAttendanceIn.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from time import sleep
+import sys
+
+# パスを通すためのimport文
+import chromedriver_binary
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://co-works.net:91/aipo/")
+
+# ユーザー名入力
+nameForm = driver.find_element_by_id("member_username")
+nameForm.send_keys("taichi.muraoka")
+
+# パスワード
+pw = driver.find_element_by_id("password")
+pw.send_keys("9999")
+
+sleep(1)
+
+# ログインボタン
+loginButton = driver.find_element_by_name("login_submit")
+loginButton.submit()
+
+sleep(2)
+
+# 出勤
+inAttend = driver.find_element_by_xpath("")
+inAttend.click()
+
+sleep(1)
+
+driver.quit()
+sys.exit()
